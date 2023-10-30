9a989edca9e33f28254c0d0ab11c7016e4511f53
Lidabdoyann <148535537+Lidabdoyann@users.noreply.github.com>
2023-10-30 18:59:59 +0400

    Add files via upload

---
+++ lec_3/main.py
@@ -0,0 +1,18 @@
+#lekcia 3
+while True:
+    s = input("yntrel gorcoxutyuny (+, -, *, /): ")
+
+    if s in ('+', '-', '*', '/'):
+        a = float(input("a = "))
+        b = float(input("b = "))
+        if s == '+':
+            print("%.2f" % (a + b))
+        elif s == '-':
+            print("%.2f" % (a - b))
+        elif s == '*':
+            print("%.2f" % (a * b))
+        elif s == '/':
+            if b != 0:
+                print("%.2f" % (a / b))
+            else:
+                print("0 i vra chi bajanvum !")
